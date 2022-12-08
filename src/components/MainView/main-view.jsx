adc64ddc7f76d5a1eb2e42733529120ab77e6937
slap-br <silvio.pelegrin@hotmail.com>
2022-12-08 06:59:14 +0100

    update

---
+++ src/components/MainView/main-view.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class MainView extends React.Component {
+
+  render() {
+    return (
+      <div className="main-view">
+        <div>Inception</div>
+        <div>The Shawshank Redemption</div>
+        <div>Gladiator</div>
+      </div>
+    );
+  }
+}
