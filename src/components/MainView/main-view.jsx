050fcbcb86c0c917f0939b1d8eaeb972a2af8d74
slap-br <silvio.pelegrin@hotmail.com>
2022-12-12 21:32:33 +0100

    update
    Api added to main-view

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
-import { MovieCard } from "../movie-card/movie-card";
-import { MovieView } from "../movie-view/movie-view";
+import { MovieCard } from "../movie-card/movie-card"
+import { MovieView } from "../movie-view/movie-view"
 
 export const MainView = () => {
   const [movies, setMovies] = useState([]);
@@ -8,37 +8,38 @@ export const MainView = () => {
   const [selectedMovie, setSelectedMovie] = useState(null);
 
   useEffect(() => {
-      fetch("smclub.herokuapp.com/movies")
+    fetch("https://smclub.herokuapp.com/movies")
       .then((response) => response.json())
-        .then((data) => {
-          const moviesFromApi = data;
-            setMovies(moviesFromApi);
+      .then((data) => {
+        const moviesFromApi = data.map((item) => {
+          return {
+            id: item._id,
+            description: item.Description,
+            image: item.ImagePath,
+            title: item.Title,
+            genre: `Name: ${item.Genre.Name} Description: ${item.Genre.Description}`
+          };
       });
-    }, []);
-    
+       setMovies(moviesFromApi)
+      });
+  }, []);
 
   if (selectedMovie) {
-    return (
-      <MovieView
-        movie={selectedMovie}
-        onBackClick={() => setSelectedMovie(null)}
-      />
-    );
-  }
-  if (movies.lenght === 0) {
-    return <div> The list is empty! </div>;
+    return ( <MovieView movie={selectedMovie} 
+    onBackClick={() => setSelectedMovie(null)} />);
   }
 
+  if(movies.length === 0) {
+    return <div> list is empty! oh no! </div>;
+  }
+  
   return (
     <div>
       {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movie={movie}
-          onMovieClick={(newSelectedMovie) => {
-            setSelectedMovie(newSelectedMovie);
-          }}
-        />
+        <MovieCard key={movie.id} movie={movie}
+        onMovieClick={(newSelectedMovie) => {
+          setSelectedMovie(newSelectedMovie);
+        }} />
       ))}
     </div>
   );
